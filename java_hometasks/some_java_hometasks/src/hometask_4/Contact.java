d2208a087226d5d3aa83b93b1d43119e9b7504d2
Yurii Chornii <chorniy.yuraa@gmail.com>
2021-03-24 21:27:08 +0200

    some new changes in homework 4 (signUp signIn signOut)

--- java_hometasks/some_java_hometasks/src/hometask_4/Contact.java
+++ java_hometasks/some_java_hometasks/src/hometask_4/Contact.java
@@ -1,11 +1,14 @@
 package hometask_4;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Contact implements Serializable {
     private static final long serialVersionUID = 1L;
     private String firstName;
     private String lastName;
+    private List<ContactField> contactFields = new ArrayList<>();
 
 
     public Contact(String firstName, String lastName) {
@@ -13,6 +16,7 @@ public class Contact implements Serializable {
         this.lastName = lastName;
     }
 
+    //todo створити метод додавання філдів
 
     @Override
     public String toString() {
--- java_hometasks/some_java_hometasks/src/hometask_4/Main.java
+++ java_hometasks/some_java_hometasks/src/hometask_4/Main.java
@@ -1,27 +1,31 @@
 package hometask_4;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
     private static final String memoryPath = "C:\\Users\\yurac\\OneDrive\\Рабочий стол\\OktenWeb\\LLab_Intent\\java_hometasks\\some_java_hometasks\\src\\hometask_4\\memory\\";
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileInputStream RegisterFileInputStream = new FileInputStream(memoryPath + "registeredUsers.ser");
+        ObjectInputStream RegisterObjectInputStream = new ObjectInputStream(RegisterFileInputStream);
+        List<User> registeredUsers = (List<User>) RegisterObjectInputStream.readObject();
 
 
         User loginedUser;
-        try{
-            FileInputStream loginedUserFileInputStream = new FileInputStream(memoryPath + "loginedUser.ser");
-            ObjectInputStream loginedUserObjectInputStream = new ObjectInputStream(loginedUserFileInputStream);
-            loginedUser = (User) loginedUserObjectInputStream.readObject();
-        }catch (Exception e){
-            e.printStackTrace();
-            loginedUser = null;
-        }
+
+        FileInputStream loginedUserFileInputStream = new FileInputStream(memoryPath + "loginedUser.ser");
+        ObjectInputStream loginedUserObjectInputStream = new ObjectInputStream(loginedUserFileInputStream);
+        loginedUser = (User) loginedUserObjectInputStream.readObject();
+
 
         System.out.println(loginedUser);
 
 //чисто тести чи створюється новий юзер і чи додаються йому контакти
-        //todo до контакків створити ше АррейЛіст з філдами!
+        //todo до контакків створити ше АррейЛіст з філдами! done
         User testUser = new User("Test", "1111");
         Contact testContact = new Contact("Petro", "Petrenko");
         testUser.addContact(testContact);
@@ -32,62 +36,121 @@ public class Main {
 
         String answer = "";
 
+        do {
+            if (loginedUser == null) {
+                System.out.println("Натисніть 1 щоб зареєструватись або 2 щоб увійти");
+                answer = bufferedReader.readLine();
 
-        try {
-            do {
-                if (loginedUser == null) {
-                    System.out.println("Натисніть 1 щоб зареєструватись або 2 щоб увійти");
+                if (answer.equals("1")) {
+
+                    System.out.println("Введіть ваш логін");
                     answer = bufferedReader.readLine();
+                    String login = answer;
 
-                    if (answer.equals("1")) {
+                    System.out.println("Введіть ваш пароль");
+                    answer = bufferedReader.readLine();
+                    String password = answer;
 
-                        System.out.println("Введіть ваш логін");
-                        answer = bufferedReader.readLine();
-                        String login = answer;
+                    User user = new User(login, password);
+                    loginedUser = user;
 
-                        System.out.println("Введіть ваш пароль");
-                        answer = bufferedReader.readLine();
-                        String password = answer;
+                    registeredUsers.add(user);
 
-                        User user = new User(login, password);
+                    FileOutputStream RegisterOutputStream = new FileOutputStream(memoryPath + "registeredUsers.ser");
+                    ObjectOutputStream RegisterObjectOutputStream = new ObjectOutputStream(RegisterOutputStream);
+                    RegisterObjectOutputStream.writeObject(registeredUsers);
+                    System.out.println("Ваш акаунт створено, і ви автоматично авторизувались");
+
+                } else if (answer.equals("2")) {
+                    System.out.println("ви натиснули 2");
+                    System.out.println("Введіть ваш логін");
+                    answer = bufferedReader.readLine();
+                    String login = answer;
+
+                    System.out.println("Введіть ваш пароль");
+                    answer = bufferedReader.readLine();
+                    String password = answer;
+
+                    User user = new User(login, password);
+
+                    if (registeredUsers.contains(user)){
                         loginedUser = user;
-
-
-
-
-
-                    } else if (answer.equals("2")) {
-                        System.out.println("ви натиснули 2");
+                        System.out.println("такий юзер знайдений, ви увійшли в свій акаунт");
+                    }else{
+                        System.out.println("такого юзера не знайдено");
                     }
-                } else if (loginedUser != null) {
-                    answer = bufferedReader.readLine();
                 }
+            } else if (loginedUser != null) {
+                System.out.println("Дії для залогіненого юзера");
+                System.out.println("введіть 9 - щоб розлогінитись");
+                answer = bufferedReader.readLine();
+                if (answer.equals("9")){
+                    loginedUser = null;
+                }
+            }
+        } while (!answer.equals("0"));
 
 
-            } while (!answer.equals("0"));
-
-
-            //создаем 2 потока для сериализации объекта и сохранения его в файл
-            FileOutputStream outputStream = new FileOutputStream(memoryPath + "memory.ser");
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-
-            // сохраняем игру в файл
-            objectOutputStream.writeObject(testUser);
-
-
-            FileInputStream fileInputStream = new FileInputStream(memoryPath + "memory.ser");
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            User user2 = (User) objectInputStream.readObject();
-
-            System.out.println(user2);
-
-            //закрываем поток и освобождаем ресурсы
-            objectOutputStream.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            do {
+//                if (loginedUser == null) {
+//                    System.out.println("Натисніть 1 щоб зареєструватись або 2 щоб увійти");
+//                    answer = bufferedReader.readLine();
+//
+//                    if (answer.equals("1")) {
+//
+//                        System.out.println("Введіть ваш логін");
+//                        answer = bufferedReader.readLine();
+//                        String login = answer;
+//
+//                        System.out.println("Введіть ваш пароль");
+//                        answer = bufferedReader.readLine();
+//                        String password = answer;
+//
+//                        User user = new User(login, password);
+//                        loginedUser = user;
+//
+//                        registeredUsers.add(user);
+//
+//                        FileOutputStream RegisterOutputStream = new FileOutputStream(memoryPath + "registeredUsers.ser");
+//                        ObjectOutputStream RegisterObjectOutputStream = new ObjectOutputStream(RegisterOutputStream);
+//                        RegisterObjectOutputStream.writeObject(registeredUsers);
+//                        System.out.println("Ваш акаунт створено, і ви автоматично авторизувались");
+//
+//                    } else if (answer.equals("2")) {
+//                        System.out.println("ви натиснули 2");
+//                    }
+//                } else if (loginedUser != null) {
+//                    answer = bufferedReader.readLine();
+//                }
+//
+//
+//            } while (!answer.equals("0"));
+//
+//
+//            //создаем 2 потока для сериализации объекта и сохранения его в файл
+//            FileOutputStream outputStream = new FileOutputStream(memoryPath + "memory.ser");
+//            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+//
+//            // сохраняем игру в файл
+//            objectOutputStream.writeObject(testUser);
+//
+//
+//            FileInputStream fileInputStream = new FileInputStream(memoryPath + "memory.ser");
+//            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+//            User user2 = (User) objectInputStream.readObject();
+//
+//            System.out.println(user2);
+//
+//            //закрываем поток и освобождаем ресурсы
+//            objectOutputStream.close();
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
 
     }
+
 }
+
